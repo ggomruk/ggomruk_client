f959fccd99ceeddb8e6f4776ec771a52a90cd246
eqfwcev123 <dohyeonee95@hotmail.com>
2024-08-20 01:03:43 +0800

    set websocket connection to exchange as provider

---
+++ app/context/websocket.context.tsx
@@ -0,0 +1,92 @@
+'use client';
+import { createContext, ReactNode, useCallback, useContext, useEffect, useState } from "react"
+
+interface IKlineData {
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    time: number;
+}
+
+interface ISymbolData {}
+
+interface IWebsocketProviderProps {
+    children: ReactNode;
+}
+
+interface IWebsocketContext {
+    klineData: IKlineData[];
+    symbolData: ISymbolData;
+    setSymbol: (symbol: string) => void;
+}
+
+const WebSocketContext = createContext<IWebsocketContext|undefined>(undefined);
+
+export const WeboscketProvider = ({children}: IWebsocketProviderProps) => {
+    const [klineData, setKlineData] = useState<IKlineData[]>([]);
+    const [symbolData, setSymbolData] = useState<ISymbolData>({});
+    const [symbol, setSymbol] = useState<string>('BTCUSDT');
+
+    const connectWebsocket = useCallback((url: string, onMessage: (event: MessageEvent) => void) => {
+
+        const ws = new WebSocket(url);
+        ws.onopen = () => {
+            console.log("Connected to " + url);
+        }
+
+        ws.onmessage = onMessage;
+
+        ws.onerror = (error) => {
+            console.log("Error: " + error);
+        }
+
+        ws.onclose = (event) => {
+            console.log("Connection closed : ", event.reason);
+            setTimeout(() => connectWebsocket(url, onMessage), 10000);
+        }
+
+        return ws;
+
+    }, [])
+
+    useEffect(() => {
+        const klineUrl = `wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@kline_1m`;
+        const symbolUrl = `wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@ticker`;
+        
+        const klineWs = connectWebsocket(klineUrl, (event: MessageEvent) => {
+            const data = JSON.parse(event.data);
+            let candleStick = data.k;
+            let d = {
+                open: parseFloat(candleStick.o),
+                high: parseFloat(candleStick.h),
+                low: parseFloat(candleStick.l),
+                close: parseFloat(candleStick.c),
+                time: candleStick.T / 1000
+            }
+            setKlineData((prevData) => {
+                const newData = [...prevData, d];
+                return newData.length > 100 ? newData.slice(newData.length - 100) : newData;
+            });
+        });
+        const symbolWs = connectWebsocket(symbolUrl, (event: MessageEvent) => {
+            const data = JSON.parse(event.data);
+            setSymbolData(data);
+        });
+
+        return () => {
+            klineWs.close();
+            symbolWs.close();
+        }
+    }, [symbol, connectWebsocket]);
+
+    return (
+        <WebSocketContext.Provider value={{klineData, symbolData, setSymbol}}>
+            {children}
+        </WebSocketContext.Provider>
+    )
+}
+
+export const useWebsocket = () => {
+    return useContext(WebSocketContext)
+}
