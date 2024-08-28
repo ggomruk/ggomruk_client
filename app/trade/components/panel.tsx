b6be15313a1b101638b0dd3d89e95556006dca48
eqfwcev123 <dohyeonee95@hotmail.com>
2024-08-28 23:28:06 +0800

    update panel

--- app/trade/components/panel.tsx
+++ app/trade/components/panel.tsx
@@ -1,19 +1,34 @@
 import { useWebsocket } from "@/app/context/websocket.context";
 import style from "@/app/style/component/panel.module.scss"
-import { faArrowCircleDown, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faArrowCircleDown, faCross, faSearch, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 const Panel = () => {
   const { symbolData, setSymbol } = useWebsocket();
   const [dropdownOpen, setDropdownOpen] = React.useState<boolean>(false);
+  const [currentPrice, setCurrentPrice] = React.useState<number>(0);
+  const [searchContent, setSearchContent] = React.useState<string>('');
+  const prevPriceRef = useRef<number>(0);
+
+  useEffect(() => {
+    if (symbolData?.lastPrice !== undefined) {
+      setCurrentPrice(parseFloat((symbolData.lastPrice).toFixed(2)));
+    }
+  }, [symbolData?.lastPrice]);
+
+  useEffect(() => {
+    if (currentPrice !== prevPriceRef.current) {
+      prevPriceRef.current = currentPrice;
+    }
+  }, [currentPrice]);
 
   const handleKeyDown = useCallback((event: KeyboardEvent) => {
     if (event.key === 'Escape' && dropdownOpen) {
       setDropdownOpen(false);
     }
-  }, [dropdownOpen])  
-
+  }, [dropdownOpen])
+  
   const handleOutsideClick = useCallback((event: MouseEvent) => {
     if (event.target instanceof HTMLElement) {
       if (!event.target.closest(`.${style['panel']}`)) {
@@ -21,6 +36,14 @@ const Panel = () => {
       }
     }
   }, [])
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchContent(e.target.value);
+  }
+
+  const resetSearchContent = () => {
+    setSearchContent('');
+  }
   
   useEffect(() => {
     if (dropdownOpen) {
@@ -30,16 +53,17 @@ const Panel = () => {
 
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('click', handleOutsideClick);
     }
   }, [dropdownOpen, handleKeyDown, handleOutsideClick]);
 
   const toggleDropDown = () => {
     setDropdownOpen(!dropdownOpen);
   }
+
   return (
     <div className={style['panel']}>
-      {/* <img className={style['symbol-logo']}/> */}
-      <div className={`${style['symbol-container']} ${style['panel-item']}`}>
+      <div className={`${style['panel-item']}}`}>
         <span className={style['symbol-name']}>{symbolData?.symbol.toUpperCase() ?? "BTCUSDT"}</span>
         <button className={style['symbol-button']} onClick={toggleDropDown}>
           <FontAwesomeIcon icon={faArrowCircleDown}></FontAwesomeIcon>
@@ -48,31 +72,84 @@ const Panel = () => {
           dropdownOpen && (
             <div className={style['dropdown-menu']}>
               <div className={style['dropdown-input-container']}>
-                <FontAwesomeIcon icon={faSearch} className={style['dropdown-input-icon']}/>
-                <input type="text" className={style['dropdown-input']} />
+                <FontAwesomeIcon 
+                  icon={faSearch} 
+                  className={style['dropdown-input-icon-search']}/>
+                <input type="text" value={searchContent} onChange={handleInputChange} className={style['dropdown-input']} />
+                <FontAwesomeIcon 
+                  icon={faXmark} 
+                  className={style['dropdown-input-icon-reset']}
+                  onClick={resetSearchContent} 
+                />
               </div>
               <hr className={style['divider']}/>
               <div className={style['dropdown-item-container']}>
                 <div className={style['dropdown-item']}onClick={() => setSymbol('BTCUSDT')}>BTCUSDT</div>
                 <div className={style['dropdown-item']}onClick={() => setSymbol('ETHUSDT')}>ETHUSDT</div>
-                <div className={style['dropdown-item']}onClick={() => setSymbol('ETHUSDT')}>SOLUSDT</div>
+                <div className={style['dropdown-item']}onClick={() => setSymbol('SOLUSDT')}>SOLUSDT</div>
               </div>
             </div>
           )
         }
       </div>
-      <span className={`${style['price']} ${style['panel-item']}`}>
-        Market Price: {symbolData?.lastPrice ?? 'NA'}
-      </span>
-      <span className={`${style['change']} ${style['panel-item']}`}>
-        Change: {symbolData?.priceChange ?? 'NA'}
-      </span>
-      <span className={`{$style['mark-price']} ${style['panel-item']}`}>
-        High: {symbolData?.highPrice ?? 'NA'}
-      </span>
-      <span className={`${style['index-price']} ${style['panel-item']}`}>
-        Low: {symbolData?.lowPrice ?? 'NA'}
-      </span>
+
+      {/* Current Price */}
+      <div className={style['panel-item']}>
+        <p className={style['panel-item-title']}>Mark Price</p>
+        <span className={currentPrice > prevPriceRef.current 
+            ? style['price-rise'] 
+            : currentPrice < prevPriceRef.current 
+              ? style['price-drop'] 
+              : style['price-stable']
+            }
+        >
+          {currentPrice.toFixed(2)}
+        </span>
+      </div>
+
+      {/* Market Change */}
+      <div className={style['panel-item']}>
+        <p className={style['panel-item-title']}>24h Change</p>
+        <span className={ symbolData?.priceChange > 0 
+          ? style['price-rise']
+          : symbolData?.priceChange < 0
+            ? style['price-drop']
+            : style['price=stable']
+          }>
+          {symbolData?.priceChange.toFixed(2) ?? 0 }
+        </span>
+      </div>
+
+      {/* Market Change % */}
+      <div className={style['panel-item']}>
+        <p className={style['panel-item-title']}>24h Change (%)</p>
+        <span className={ symbolData?.priceChangePercent > 0 
+          ? style['price-rise']
+          : symbolData?.priceChangePercent < 0
+            ? style['price-drop']
+            : style['price=stable']
+          }>
+          {symbolData?.priceChangePercent 
+            ? `${symbolData?.priceChangePercent.toFixed(2)}%`
+            : '0%' }
+        </span>
+      </div>
+
+      {/* High */}
+      <div className={style['panel-item']}>
+        <p className={style['panel-item-title']}>24h High</p>
+        <span className={`${style['price-stable']}`}>
+          {symbolData?.highPrice.toFixed(2) ?? 0 }
+        </span>
+      </div>
+
+      {/* Low */}
+      <div className={style['panel-item']}>
+        <p className={style['panel-item-title']}>24h Low</p>
+        <span className={`${style['price-stable']}`}>
+          {symbolData?.lowPrice.toFixed(2) ?? 0 }
+        </span>
+      </div>
     </div>
   )
 }
