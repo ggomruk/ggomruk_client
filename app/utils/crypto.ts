1a6da1046e2647986beac5015452f72736bf34bd
eqfwcev123 <dohyeonee95@hotmail.com>
2024-09-05 00:25:23 +0800

    list of cryptos (constants)

---
+++ app/utils/crypto.ts
@@ -0,0 +1,15 @@
+
+const cryptoList = [
+    'BTCUSDT',
+    'ETHUSDT',
+    'SOLUSDT',
+    'XRPUSDT',
+    'AVAXUSDT',
+    'DOGEUSDT',
+    'SHIBUSDT',
+    'LINKUSDT',
+    'ADAUSDT',
+    'TRXUSDT'
+]
+
+export default cryptoList;
