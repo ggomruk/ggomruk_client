50e8cb5365a01e8cd6adab181057682f0c7c48c6
eqfwcev123 <dohyeonee95@hotmail.com>
2024-09-05 00:26:56 +0800

    fix websocket to handle duplicate connections (same subscribtion subscribed twice issue)

--- app/context/websocket.context.tsx
+++ app/context/websocket.context.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { createContext, ReactNode, useCallback, useContext, useEffect, useState } from "react"
+import { createContext, ReactNode, useCallback, useContext, useEffect, useRef, useState } from "react"
 
 interface IKlineData {
     open: number;
@@ -18,6 +18,7 @@ interface ISymbolData {
     openPrice: number;
     highPrice: number;
     lowPrice: number;
+    quantity: number;
 }
 
 interface IWebsocketProviderProps {
@@ -30,82 +31,124 @@ interface IWebsocketContext {
     setSymbol: (symbol: string) => void;
 }
 
-const WebSocketContext = createContext<IWebsocketContext|undefined>(undefined);
+const WebSocketContext = createContext<IWebsocketContext | undefined>(undefined);
 
-export const WeboscketProvider = ({children}: IWebsocketProviderProps) => {
+export const WebsocketProvider = ({ children }: IWebsocketProviderProps) => {
     const [klineData, setKlineData] = useState<IKlineData[]>([]);
     const [symbolData, setSymbolData] = useState<ISymbolData | null>(null);
     const [symbol, setSymbol] = useState<string>('BTCUSDT');
-
-    const connectWebsocket = useCallback((url: string, onMessage: (event: MessageEvent) => void) => {
-
-        const ws = new WebSocket(url);
-        ws.onopen = () => {
-            console.log("Connected to " + url);
-        }
-
-        ws.onmessage = onMessage;
-
-        ws.onerror = (error) => {
-            console.log("Error: " + error);
-        }
-
-        ws.onclose = (event) => {
-            console.log("Connection closed : ", event.reason);
-            setTimeout(() => connectWebsocket(url, onMessage), 10000);
-        }
-
-        return ws;
-
-    }, [])
+    const websocketRef = useRef<WebSocket | null>(null);
 
     useEffect(() => {
-        const klineUrl = `wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@kline_1m`;
-        const symbolUrl = `wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@ticker`;
+        function connectWebsocket(onMessage: (event: MessageEvent) => void) {
+            console.log('Callback called')
+            const websocketUrl = `wss://stream.binance.com:9443/ws`;
+            const ws = new WebSocket(websocketUrl);
+
+            ws.onopen = () => {
+                const subscribe = {
+                    method: 'SUBSCRIBE',
+                    params: [
+                        `${symbol.toLowerCase()}@kline_1m`,
+                        `${symbol.toLowerCase()}@ticker`
+                    ],
+                    id: 1
+                };
+                console.log("[onOpen] Subscribed to: ", subscribe);
+                ws.send(JSON.stringify(subscribe));
+            };
+
+            ws.onmessage = onMessage;
+
+            ws.onerror = (error) => {
+                console.log("[onError] Error: " + error);
+            };
+
+            ws.onclose = (event) => {
+                console.log("[onClose] WebSocket connection closed: ", event.reason);
+                // Do not reconnect immediately in onclose; handle reconnection externally if needed
+            };
+
+            return ws;
+        }
+
+        // create websocket connection
+        console.log("[websocket][start] Creating websocket connection")
         
-        const klineWs = connectWebsocket(klineUrl, (event: MessageEvent) => {
+        websocketRef.current = connectWebsocket((event: MessageEvent) => {
             const data = JSON.parse(event.data);
-            let candleStick = data.k;
-            let d = {
-                open: parseFloat(candleStick.o),
-                high: parseFloat(candleStick.h),
-                low: parseFloat(candleStick.l),
-                close: parseFloat(candleStick.c),
-                time: candleStick.T / 1000
+            console.log(data.s)
+            if (data.e === "kline") {
+                let d = {
+                    open: parseFloat(data.k.o),
+                    high: parseFloat(data.k.h),
+                    low: parseFloat(data.k.l),
+                    close: parseFloat(data.k.c),
+                    time: data.k.T / 1000,
+                };
+                setKlineData((prevData) => {
+                    const newData = [...prevData, d];
+                    return newData.length > 100 ? newData.slice(newData.length - 100) : newData;
+                });
+            } else if (data.e === "24hrTicker") {
+                setSymbolData({
+                    eventTime: data.E,
+                    symbol: data.s,
+                    priceChange: parseFloat(data.p),
+                    priceChangePercent: parseFloat(data.P),
+                    lastPrice: parseFloat(data.c),
+                    openPrice: parseFloat(data.o),
+                    highPrice: parseFloat(data.h),
+                    lowPrice: parseFloat(data.l),
+                    quantity: parseFloat(data.q),
+                });
+            } else {
+                console.log("[websocket] Received unknown message: ", data);
             }
-            setKlineData((prevData) => {
-                const newData = [...prevData, d];
-                return newData.length > 100 ? newData.slice(newData.length - 100) : newData;
-            });
-        });
-        const symbolWs = connectWebsocket(symbolUrl, (event: MessageEvent) => {
-            const data = JSON.parse(event.data);
-            setSymbolData({
-                eventTime: data.E,
-                symbol: data.s,
-                priceChange: parseFloat(data.p),
-                priceChangePercent: parseFloat(data.P),
-                lastPrice: parseFloat(data.c),
-                openPrice: parseFloat(data.o),
-                highPrice: parseFloat(data.h),
-                lowPrice: parseFloat(data.l),
-            });
         });
 
         return () => {
-            klineWs.close();
-            symbolWs.close();
+            websocketRef.current?.close();
+            websocketRef.current = null;
         }
-    }, [symbol, connectWebsocket]);
+
+
+        // return () => {
+        //     console.log("[websocket][cleanup] Cleaning up WebSocket connection");
+        //     if (websocketRef.current) {
+        //         console.log("Websocket is open => ", websocketRef.current.readyState === WebSocket.OPEN)
+        //         if (websocketRef.current.readyState === WebSocket.OPEN) {
+        //             console.log("2. Unsubscribing and closing WebSocket connection");
+        //             const unsubscribe = {
+        //                 method: "UNSUBSCRIBE",
+        //                 params: [
+        //                     `${symbol.toLowerCase()}@kline_1m`,
+        //                     `${symbol.toLowerCase()}@ticker`,
+        //                 ],
+        //                 id: 1,
+        //             };
+        //             console.log("[websocket] Unsubscribing from: ", unsubscribe);
+        //             websocketRef.current.send(JSON.stringify(unsubscribe));
+        //             websocketRef.current.close();
+        //         }
+        //     }
+        //     websocketRef.current = null;
+        //     setKlineData([]);
+        //     setSymbolData(null);
+        //     console.log("Reset Called")
+        //     console.log("\n")
+        //     console.log("\n")
+        // };
+    }, [symbol]);
 
     return (
-        <WebSocketContext.Provider value={{klineData, symbolData, setSymbol}}>
+        <WebSocketContext.Provider value={{ klineData, symbolData, setSymbol }}>
             {children}
         </WebSocketContext.Provider>
-    )
+    );
 }
 
-export const useWebsocket = () : IWebsocketContext=> {
+export const useWebsocket = (): IWebsocketContext => {
     const context = useContext(WebSocketContext);
     if (context === undefined) {
         throw new Error('useWebsocket must be used within a WebSocketProvider');
